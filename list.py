0bac2c641a8a1cef85cad70e8bfc989b0baed494
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-01-18 10:20:50 +0600

    list

---
+++ list.py
@@ -0,0 +1,73 @@
+# A list is a collection which is orderd nad changeable . allow duplicate number
+
+#Create list
+
+# numbers = [1,2,3,4,5]
+
+# print (numbers)
+
+# Using constructor
+
+numbers = list((1,2,3,4,5))
+# print (numbers)
+
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+#position check
+
+#Get value
+# print(fruits[1])
+
+#Get length
+
+#print(len(fruits))
+
+# append to list 
+
+#fruits.append('Mangose')
+
+#print(fruits)
+
+#Remove from list
+
+#fruits.remove('Apples')
+
+#print(fruits)
+
+#Insert into position
+
+'''fruits.insert(2,'Strawberries')
+
+print (fruits)'''
+
+
+#Remove from position
+
+'''fruits.pop(3)
+
+print (fruits)'''
+
+
+#Reverse List
+
+'''fruits.reverse()
+
+print(fruits)'''
+
+#sort List
+
+'''fruits.sort()
+
+print(fruits)'''
+
+
+#Reverse sort
+
+'''fruits.sort(reverse=True)
+
+print(fruits)'''
+
+
+
+
+
