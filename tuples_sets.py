3bfde8759c35de0f47419b1dbe1bd758acc513c4
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-01-18 21:14:09 +0600

    tuple set

---
+++ tuples_sets.py
@@ -0,0 +1,58 @@
+#A Tuple is a collection which is orderd and unchangeable allows duplicate members.
+
+#Simple tuple
+
+fruit_tuple =('Apple', 'Orange','Mango')
+
+#using constructor
+
+#fruit_tuple = tuple(('Apple', 'Orange','Mango))
+
+# print(fruit_tuple)
+
+# Get single value
+print (fruit_tuple[1])
+
+#Can not change value
+# fruit_tuple[1] = 'Grape'
+
+
+# Tuples with one value should have trailing comma
+
+# fruit_tuple2 =('Apple')
+
+# del fruit_tuple2
+
+#Get length of tuple 
+
+# print (len(fruit_tuple2))
+
+
+#A Set is a collection which is unorderd and unindexed . no duplicate members
+
+#Create set
+
+fruit_set = {'Apple', 'Orange', 'Mango',  }
+
+# print (fruit_set)
+
+#Check if in set
+
+# print ('Apple' in fruit_set)
+
+# add to set
+
+# fruit_set.add('Grape')
+
+#Remove something
+
+# fruit_set.remove('Grape')
+
+#if we want to clear
+# fruit_set.clear()
+
+#delete set
+# del fruit_set
+
+# print(fruit_set)
+
