455f8759769e0daab35cecbeb481646a24fe5504
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-01-20 10:55:23 +0600

    function

--- function.py
+++ function.py
@@ -3,25 +3,50 @@
 
 
 # Create function
-def  sayHello(name):
 
-    """
- Print hello and then name
-    """
-   print('Hello' + name)
+ # we also can pass argument like way name ='miraz'
 
-sayHello('sanjose')
+# def sayHello(name):
+    
+    #  Prints hello and then name
+    
+#     print ('Hello'+ name)
 
-#Return
-
-def sum(num1,num2):
-    total = num1+num2
-    return total
+# sayHello('Miraz')
 
 
- sum(34,45)
- 
- print (sum)
+#Return value
+
+# def getSum(num1, num2):
+#     total = num1 + num2
+#     return total
+
+# print(getSum(34,45))
+#we also can
+
+# numSum = getSum(3,4)
+
+# print(numSum)
+
+
+# def addOneToNum(num):
+#     num +=1
+#     return num
+
+# num = 5
+
+# new_num = addOneToNum(num)
+# print (new_num)
+
+getSum = lambda num1, num2: num1 + num2
+
+print(getSum(9,2))
+
+addOneToNum = lambda num: num + 1
+
+print (addOneToNum(5))
+
+
 
 
 
